165b2b54d5d0684b7d0fbd0fd05eb9c060a3daf8
hjyang369 <ghlwls6@gmail.com>
2024-01-16 13:04:51 +0900

    greedy baekjoon 2839

--- 회진/greedy/baekjoon/2839/solution.js
+++ 회진/greedy/baekjoon/2839/solution.js
@@ -1,3 +1,28 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim();
+
+const A = 5;
+const B = 3;
+let num = Number(input);
+const arr = [0, num / A, num / B];
+
+while (num > 0) {
+  if (num >= A) {
+    num -= A;
+    arr[0]++;
+  } else if (num >= B) {
+    num -= B;
+    arr[0]++;
+  } else {
+    arr[0] = -1;
+    break;
+  }
+}
+
+const answer = arr.filter((num) => Number.isInteger(num) && num > 0);
+if (answer.length > 0) {
+  return console.log(Math.min(...answer));
+} else {
+  return console.log(-1);
+}
