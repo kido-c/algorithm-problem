d746f2e5ec77c4618f2745852a7ddc03aa5619ab
danakim530 <danakim530@gmail.com>
2024-01-24 23:33:52 +0900

    TwoPointer & binarySearch

--- 다정/3_binarySearch/입국심사/solution.js
+++ 다정/3_binarySearch/입국심사/solution.js
@@ -1,21 +1,10 @@
-// 2839. 설탕 배달 
-// 상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
-// 상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
-// 상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
-
-const fs = require("fs");
-let input = fs
-  .readFileSync(__dirname + "/input.txt")
-  .toString()
-  .trim();
-
-// 3kg, 5kg
-let aCount = 0;
-let bCount = 0;
-
-if(input%3 !== 0 ) console.log(-1);
-aCount = Math.floor(input / 5);
-bCount = Math.floor((input % 5) / 3);
-console.log(aCount + bCount);
-
-// 예제 3,4,5가 답과 다르게 출력되는데 이유를 모르겠음. 문제를 잘못 이해한 것 같다. 
+// 입국 심사
+function solution(n, times) {
+  times.sort((a,b)=> a-b);
+   let leastTime = n * times[0];
+   let mostTime = n * times[times.length-1];
+   
+   //감이 안온다
+   return leastTime
+   
+}
