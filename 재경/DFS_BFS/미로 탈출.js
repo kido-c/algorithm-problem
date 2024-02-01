4f13a5b7a09ef0433c619fd36a638f7152ac2692
ujkey <135115849+ujkey@users.noreply.github.com>
2024-02-01 10:14:13 +0900

    Update: BFS > 미로 탈출
    입력값 수정하여 올바른 정답 출력 확인 완료

--- 재경/DFS_BFS/미로 탈출.js
+++ 재경/DFS_BFS/미로 탈출.js
@@ -1,6 +1,6 @@
 // BFS > 미로 탈출
 // https://www.youtube.com/watch?v=7C9RgOcvkvo
-// not resolved yet
+// resolved
 
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const resource = require("fs")
@@ -10,8 +10,6 @@ const resource = require("fs")
 
 const [N, M] = resource[0].split(" ").map(Number);
 const maze = resource.slice(1).map((v) => v.split("").map(Number));
-
-// console.log(N, M, maze);
 
 function solution(N, M, maze) {
   const queue = [];
@@ -30,14 +28,14 @@ function solution(N, M, maze) {
     for (let i = 0; i < 4; i++) {
       const nx = x + dx[i]; // 다음 x 좌표
       const ny = y + dy[i]; // 다음 y 좌표
-      // console.log(nx, ny);
 
       // 범위를 벗어난 경우
       if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
       // 장애물인 경우(괴물이 있는 경우, 0)
       if (maze[nx][ny] === 0) continue;
+
       // 열린 공간인 경우(괴물이 없는 경우, 1)
-      if (maze[nx][ny] !== 0) {
+      if (maze[nx][ny] === 1) {
         console.log("fsdfsd", nx, ny, maze[nx][ny]);
         maze[nx][ny] = maze[x][y] + 1; // 이동한 칸의 개수를 이전 칸의 개수 + 1로 설정
         queue.push([nx, ny]); // 다음 좌표를 큐에 삽입
@@ -50,7 +48,6 @@ function solution(N, M, maze) {
 
 solution(N, M, maze);
 
-// 왜 10이 안나오고 0이 나오는가?
 // 1. queue에 [0, 0]을 넣고 시작 (queue.push([x, y]))
 // 2. [0, 0]을 shift하고, 상하좌우 좌표를 구함 (const nx = x + dx[i], const ny = y + dy[i])
 // 3. 좌표가 범위를 벗어나지 않고, 벽이 아니고, 이동할 수 있는 경우 (if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;)
