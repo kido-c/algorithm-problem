0085a520be511cacc9f3b8a99abc0d403e25b9c5
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-02-15 15:19:06 +0900

    타겟 넘버

---
+++ 기석/bfs/타겟넘버/solution.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+  var answer = 0;
+  const length = numbers.length;
+
+  function dfs(count, sum) {
+    if (count === length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+    dfs(count + 1, sum + numbers[count]);
+    dfs(count + 1, sum - numbers[count]);
+  }
+  dfs(0, 0);
+  return answer;
+}
