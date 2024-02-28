25746293805d7f999833dda477beec5d23e3209c
hjyang369 <ghlwls6@gmail.com>
2024-02-29 01:49:43 +0900

    이코테 왕실의 나이트

---
+++ 회진/구현/baekjoon/10431/solution.js
@@ -0,0 +1,7 @@
+// 줄세우기
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const N = Number(input[0]);
+
+console.log(input);
